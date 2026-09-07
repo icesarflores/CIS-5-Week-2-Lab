fb1a2dcca21576321e6e7d7b9085c425e2de0ba8
Cesar Flores <14372814+icesarflores@users.noreply.github.com>
2026-09-07 21:48:04 +0000

    Updated code

--- main.cpp
+++ main.cpp
@@ -7,10 +7,12 @@ int main() {
 using namespace std;
 
 cout << "=== About Me === \n";
-cout << "Name: \n";
-cout << "Interests: \n";
-cout << "CIS-5 Goals: \n";
-cout << "Tool Path: \n";
+cout << "Name: Cesar Flores\n";
+cout << "Interests: Technology, writing, movies, music, art, painting. \n";
+cout << "CIS-5 Goals: I'm looking to become proficient in C++. I feel that by understanding coding structure and the process will help me with other coding languages.\n";
+cout << "Tool Path: Github codespace and VSCode. Also, different PC's to better learn git commands such as commit, push, pull, etc. \n";
+cout << "Fun Fact: I bought my first coding book, \"JavaScript for the World Wide Web: Visual QuickStart Guide, 3rd Edition in 1999, to learn code.\n";
+cout << "\n";
 
 return 0;
 }
