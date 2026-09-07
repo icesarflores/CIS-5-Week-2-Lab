af9a8ab1798e3219c3b443d38646242d6a17550a
Cesar Flores <14372814+icesarflores@users.noreply.github.com>
2026-09-07 21:55:21 +0000

    Updates

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ cout << "Name: Cesar Flores\n";
 cout << "Interests: Technology, writing, movies, music, art, painting. \n";
 cout << "CIS-5 Goals: I'm looking to become proficient in C++. I feel that by understanding coding structure and the process will help me with other coding languages.\n";
 cout << "Tool Path: Github codespace and VSCode. Also, different PC's to better learn git commands such as commit, push, pull, etc. \n";
-cout << "Fun Fact: I bought my first coding book, \"JavaScript for the World Wide Web: Visual QuickStart Guide, 3rd Edition in 1999, to learn code.\n";
+cout << "Fun Fact: I bought my first coding book, \"JavaScript for the World Wide Web: Visual QuickStart Guide, 3rd Edition in 1999\", to learn code." << endl;
 cout << "\n";
 
 return 0;
