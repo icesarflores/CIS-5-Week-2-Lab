2f033708692f944e58b54a5708e4c63d477326d6
Cesar Flores <14372814+icesarflores@users.noreply.github.com>
2026-09-07 21:20:42 +0000

    Updates to code

--- main.cpp
+++ main.cpp
@@ -11,12 +11,6 @@ cout << "Name: \n";
 cout << "Interests: \n";
 cout << "CIS-5 Goals: \n";
 cout << "Tool Path: \n";
-cout << "\n";
-cout << "=== Week 02 Goals === \n";
-cout << "I can point at include, main, a statement and a comment. \n";
-cout << "I can compile \n";
-cout << "I can fix the first compile error and rebuild." << endl;
 
- 
 return 0;
 }
